fd6efac51c8b1df05e401e118abb1f3ddff5fccb
Mehdi Q. <79559773+mehdiq1980@users.noreply.github.com>
2026-08-20 00:11:45 +0330

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,55 @@
+export async function loadMenu(){
 
+    const container =
+        document.getElementById("menuContainer");
+
+
+    if(!container) return;
+
+
+    const response = await fetch(
+        "./components/menu.html?v=20260820-01",
+        {
+            cache:"no-store"
+        }
+    );
+
+
+    container.innerHTML =
+        await response.text();
+
+
+
+    const menuButton =
+        document.getElementById("menuButton");
+
+
+    const menu =
+        document.getElementById("menu");
+
+
+
+    if(menuButton && menu){
+
+        menuButton.addEventListener(
+            "click",
+            ()=>{
+
+                const opened =
+                    menu.hidden;
+
+
+                menu.hidden = opened;
+
+
+                menuButton.setAttribute(
+                    "aria-expanded",
+                    String(opened)
+                );
+
+            }
+        );
+
+    }
+
+}
