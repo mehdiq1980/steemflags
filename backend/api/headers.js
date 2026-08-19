0ee8f044aa66b7c7092fa9e6edd2bc2072c63503
Mehdi Q. <79559773+mehdiq1980@users.noreply.github.com>
2026-08-19 04:01:52 +0330

    Remove obsolete external backend

--- backend/api/headers.js
+++
@@ -1,8 +0,0 @@
-export const JSON_HEADERS = Object.freeze({
-  'content-type': 'application/json; charset=utf-8',
-  'cache-control': 'no-store'
-});
-
-export function withJsonHeaders(response) {
-  return { ...response, headers: { ...JSON_HEADERS, ...(response?.headers || {}) } };
-}
