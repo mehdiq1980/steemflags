a4b48c24942d87bb1371ac155427b0a158ffcf12
Mehdi Q. <79559773+mehdiq1980@users.noreply.github.com>
2026-09-04 18:33:46 +0330

    Bust auth module cache after login timeout fix

--- js/app-v2.js
+++ js/app-v2.js
@@ -1,6 +1,6 @@
 import { FlagGame } from './game.js';
 import { applyLanguage, getLanguage, t, setLanguage } from './i18n.js';
-import { verifyPostingKey } from './steem-auth.js?v=20260904-login-fix-08';
+import { verifyPostingKey } from './steem-auth.js?v=20260904-login-fix-09';
 import { saveGameResult } from './reward.js';
 import { loadMenu } from './menu.js';
 
