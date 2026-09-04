56d989e4476aba43bd06ea83f379f423921fe029
Mehdi Q. <79559773+mehdiq1980@users.noreply.github.com>
2026-09-04 18:51:11 +0330

    Bust auth module cache after dsteem fix

--- js/app-v2.js
+++ js/app-v2.js
@@ -1,6 +1,6 @@
 import { FlagGame } from './game.js';
 import { applyLanguage, getLanguage, t, setLanguage } from './i18n.js';
-import { verifyPostingKey } from './steem-auth.js?v=20260904-login-fix-09';
+import { verifyPostingKey } from './steem-auth.js?v=20260904-login-fix-10';
 import { saveGameResult } from './reward.js';
 import { loadMenu } from './menu.js';
 
